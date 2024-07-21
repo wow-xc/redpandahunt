c285982f3bf1f5c01b46cee7b332efad4dbb90f6
wow-xc <wonheelee20081203@gmail.com>
2024-07-21 23:01:44 +0900

    dsf

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function showMole() {
     const holes = document.querySelectorAll('.hole');
     const randomHole = holes[Math.floor(Math.random() * holes.length)];
     const randomMoleType = moleTypes[Math.floor(Math.random() * moleTypes.length)];
-
+    
     const mole = document.createElement('div');
     mole.classList.add('mole', randomMoleType);
     randomHole.appendChild(mole);
@@ -28,3 +28,10 @@ function showMole() {
 }
 
 setInterval(showMole, 1500);
+
+// 커서 이미지 이동 처리
+document.addEventListener('mousemove', (e) => {
+    const customCursor = document.getElementById('custom-cursor');
+    customCursor.style.left = `${e.clientX}px`;
+    customCursor.style.top = `${e.clientY}px`;
+});
